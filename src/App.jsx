35a07540b35b39718df2eb6f35c8f601eb07646c
sanimohammedbalami-hash <sanimohammedbalami@gmail.com>
2026-09-10 19:43:55 +0100

    Update routing: SplashScreen -> OnboardingScreen -> Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SplashScreen from './pages/SplashScreen';
+import OnboardingScreen from './pages/OnboardingScreen';
 import Home from './pages/customer/Home';
 import Header from './components/common/Header';
 import BottomNav from './components/common/BottomNav';
 
 export default function App() {
-  const [showSplash, setShowSplash] = useState(true);
-
-  if (showSplash) {
-    return <SplashScreen onFinish={() => setShowSplash(false)} />;
-  }
-
   return (
     <Router>
       <div className="min-h-screen bg-gray-50 pb-16 md:pb-0">
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </main>
-        <BottomNav />
+        <Routes>
+          <Route path="/" element={<SplashScreen />} />
+          <Route path="/onboarding" element={<OnboardingScreen />} />
+          <Route path="/home" element={<><Header /><Home /><BottomNav /></>} />
+        </Routes>
       </div>
     </Router>
   );
